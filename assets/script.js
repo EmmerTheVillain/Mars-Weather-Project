f3ca0af1e194c943fbe7838ff8daa01d35022ed4
GuyD15 <guywayne.domino7@gmail.com>
2023-06-12 19:06:01 -0500

    Added buttons and slideshow function code

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,8 @@ var apiKey = 'qHqc0M7bu2zPLS268h6g49uEUPPzQ6LT13FYybRA';
 var weatherContainer = document.getElementById('weatherContainer');
 var roverForm = document.getElementById('roverForm');
 var photosContainer = document.getElementById('photosContainer');
-var dateInputEl = $('#dateInput');
+var weatherDate = document.getElementById('weatherDate');
+var currentPhotoIndex = 0;
 
 // Function to fetch Mars weather data
 var fetchMarsWeather = () => {
@@ -12,7 +13,8 @@ var fetchMarsWeather = () => {
   fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-        var weather = data.weather;
+        var latestSol = data.sol_keys[data.sol_keys.length -1];
+        var weather = data[latestSol];
         weatherContainer.innerHTML = '';
 
         for (var [key, value] of Object.entries(weather)) {
@@ -31,6 +33,9 @@ var fetchMarsWeather = () => {
             weatherDetailE1.appendChild(valueE1);
             weatherContainer.appendChild(weatherDetailE1);
         }
+
+        var date = dayjs(latestSol).format('MMMM D, YYYY');
+        weatherDate.textContent = date;
     })
     .catch(error => {
       console.log('Error fetching Mars weather:', error);
@@ -44,31 +49,45 @@ var fetchRoverPhotos = (rover, date) => {
   fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-        var photos = data.photos;
+        var roverPhotos = data.photos;
         photosContainer.innerHTML = '';
 
         // Limit the photos to 10 for example
-        photos = photos.slice(0, 5);
+        var limitedPhotos = photos.slice(0, 5);
 
-        photos.forEach(photo => {
+        limitedPhotos.forEach((photo, index) => {
             var photoUrl = photo.img_src;
             var roverName = photo.rover.name;
             var cameraName = photo.camera.full_name;
+
+            var slideE1 = document.createElement('div');
+            slideE1.classList.add('slide');
 
             var imgE1 = document.createElement('img');
             imgE1.src = photoUrl;
             imgE1.alt = `${roverName} - ${cameraName}`;
             imgE1.classList.add('photo'); // move the 'photo' class to the img element
-
+            slideE1.appendChild(imgE1);
             photosContainer.appendChild(imgE1); // append img directly to photosContainer
         });
+        showSlide(currentPhotoIndex);
     })
     .catch(error => {
       console.log('Error fetching rover photos:', error);
     });
 };
 
-  
+var showSlide = (index) => {
+  var slides = document.querySelectorAll('.slide');
+
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+};
   // Event listener for the rover form submission
   roverForm.addEventListener('submit', event => {
     event.preventDefault();
@@ -87,5 +106,29 @@ var fetchRoverPhotos = (rover, date) => {
     fetchRoverPhotos(selectedRover, selectedDate);
     
   });  
+
+  document.getElementById('nextButton').addEventListener('click', () => {
+    currentPhotoIndex++;
+    var slides = document.querySelectorAll('.slide');
+
+    if (currentPhotoIndex >= slides.length) {
+      currentPhotoIndex = 0;
+    }
+
+    showSlide(currentPhotoIndex);
+
+  });
+
+  document.getElementById('prevButton').addEventListener('click', () => {
+    currentPhotoIndex--;
+    var slides = document.querySelectorAll('.slide');
+
+    if (currentPhotoIndex < 0) {
+      currentPhotoIndex = slides.length -1;
+    }
+
+    showSlide(currentPhotoIndex);
+
+  });
   // Call the function to fetch Mars weather on page load
   fetchMarsWeather();
