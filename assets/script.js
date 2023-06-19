a1cdcbd8c8d0fa58483c37adf4a9c6d21b1392d7
Mike Barber <bandofgold1@gmail.com>
2023-06-19 18:56:25 -0400

    made map work

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,8 @@ var dateInputEl = $('#dateInput');
 var hisBtn = $('.historyCard');
 var currentPhotoIndex = 0;
 var errorMessage = document.getElementById('errorMessage');
+var latitudeEl 
+var longitudeEl
 
 // Function to fetch Mars weather data
 var fetchSpaceEvents = () => {
@@ -45,6 +47,14 @@ var fetchSpaceEvents = () => {
       console.log('Error fetching astronauts:', error);
     });
 };
+var map = L.map('map', {
+  center: [0,0],
+  zoom: 1
+});
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 5,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 var trackISSLocation = () => {
   var apiUrl = 'http://api.open-notify.org/iss-now.json';
@@ -61,34 +71,20 @@ var trackISSLocation = () => {
       console.log(longitude);
       var locationInfo = document.getElementById('locationInfo');
       // locationInfo.innerHTML = '';
+      latitudeEl = latitude;
+      longitudeEl = longitude;
 
-      var coordinatesE1 = document.createElement('div');
-      coordinatesE1.classList.add('coordinates');
-
-      var latitudeE1 = document.createElement('span');
-      latitudeE1.textContent = 'Latitude: ' + latitude;
-
-      var longitudeE1 = document.createElement('span');
-      longitudeE1.textContent = 'Longitude: ' + longitude;
-
-      coordinatesE1.appendChild(latitudeE1);
-      coordinatesE1.appendChild(longitudeE1);
-      locationInfo.appendChild(coordinatesE1);
-      L.marker([latitude, longitude]).addTo(map);
+      // coordinatesE1.appendChild(latitudeE1);
+      // coordinatesE1.appendChild(longitudeE1);
+      // locationInfo.appendChild(coordinatesE1);
+      var marker = L.marker([latitudeEl, longitudeEl]).addTo(map);
     })
     .catch(error => {
       console.log('Error fetching ISS location:', error);
     });
 };  
-var map = L.map('map', {
-  center: [0,0],
-  zoom: 1
-});
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 5,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
 
+// var marker = L.marker([latitudeEl, longitudeEl]).addTo(map);
 
 
 
